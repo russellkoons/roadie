01b60e9f0b122efceabd0e4d6caf8257295687c3
russellkoons <rkoons@gmail.com>
2020-02-12 07:51:53 -0500

    initial commit

---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Mockup from './assets/Software-Box-Mock-Up.jpg'
+
+const Product = () => {
+  return (
+    <div>
+      <img src={Mockup} alt="Roadie"/>
+    </div>
+  );
+}
+
+export default Product;
