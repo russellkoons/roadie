4cd96bd91fbc39cf473b85ea4f624b94c7b60015
russellkoons <rkoons@gmail.com>
2020-02-12 09:08:17 -0500

    started styling product

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,8 +3,19 @@ import Mockup from './assets/Software-Box-Mock-Up.jpg'
 
 const Product = () => {
   return (
-    <div>
-      <img src={Mockup} alt="Roadie"/>
+    <div className="product">
+      <header></header>
+      <div className="image">
+        <img src={Mockup} alt="Roadie"/>
+      </div>
+      <div classname="desc">
+        <h1>ROADIE COMMUNICATOR - INCLUDES INSTALLATION SOFTWARE</h1>
+        <p>by <strong>Roadie</strong></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <ul>
+          <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
+        </ul>
+      </div>
     </div>
   );
 }
