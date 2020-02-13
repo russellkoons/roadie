4b6cad0059defb99b4ff02e0bcb2d46314c8c00f
Koons <RUSSELL_KOONS@homedepot.com>
2020-02-13 12:29:28 -0500

    ready to submit

--- src/Product.js
+++ src/Product.js
@@ -30,9 +30,17 @@ const Product = props => {
                   &times;
                 </button>
                 <h2 style={{textAlign: 'center'}}>ADD REVIEW</h2>
-                <form style={{textAlign: 'left'}}>
+                <form onSubmit={e => {
+                  e.preventDefault();
+                  props.add({
+                    user: document.getElementById('user').value,
+                    title: document.getElementById('title').value,
+                    rating: document.getElementById('rating').value,
+                    review: document.getElementById('review').value,
+                    date: moment().format('LL')
+                  }, close)}}  style={{textAlign: 'left'}}>
                   <div className="forminput">
-                    <label for="rating">Rating</label>
+                    <label htmlFor="rating">Rating</label>
                     <select id="rating" className="greyborder">
                       <option value="1">One star</option>
                       <option value="2">Two stars</option>
@@ -42,26 +50,20 @@ const Product = props => {
                     </select>
                   </div>
                   <div className="forminput">
-                    <label for="user">Your name</label>
+                    <label htmlFor="user">Your name</label>
                     <input id="user" type="text" className="full greyborder" placeholder="Enter text here..." required />
                   </div>
                   <div className="forminput">
-                    <label for="title">Review title</label>
+                    <label htmlFor="title">Review title</label>
                     <input id="title" type="text" className="full greyborder" placeholder="Enter text here..." required />
                   </div>
                   <div className="forminput">
-                    <label for="review">Write your review below</label>
+                    <label htmlFor="review">Write your review below</label>
                     <textarea id="review" className="full greyborder" placeholder="Enter text here..." required />
                   </div>
                   <div style={{textAlign: "right"}}>
                     <button className="leftbutton upperbutton" onClick={close}>CANCEL</button>
-                    <input type="submit" className="rightbutton upperbutton" onClick={() => props.add({
-                      user: 'Test',
-                      title: 'Test',
-                      rating: '5',
-                      review: 'Blah blah blah blah blah',
-                      date: moment().format('LL')
-                    }, close)} value="SUBMIT" />
+                    <input type="submit" className="rightbutton upperbutton" value="SUBMIT" />
                   </div>
                 </form>
               </div>
@@ -69,7 +71,6 @@ const Product = props => {
             </Popup>
           <button className="upperbutton rightbutton">ADD TO CART</button>
         </div>
-        
       </div>
     </div>
   );
